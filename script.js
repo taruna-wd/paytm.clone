c2f4386818787d28636f9dd966aa621c70ef09a9
taruna-wd <tarunarajoriya18@gmail.com>
2024-01-11 13:36:30 +0530

    paytm.clone

---
+++ script.js
@@ -0,0 +1,11 @@
+// const btn2 =document.querySelector("#btn_2");
+// const btn1 =document.querySelector("#btn_1");
+// const images = document.querySelector(".right")
+
+// btn1.addEventListener("click",()=>{
+//   console.dir(btn1)
+//   // const btn1 =document.querySelector("#btn_1");
+//   btn2.style.diplay ="flex" ;
+//   btn1.style.diplay= "none";
+//   images.style.overflow = 'visible';
+// })
