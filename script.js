3262ca4d6990ee6744859f743bf48bc2e25f74a7
taruna-wd <tarunarajoriya18@gmail.com>
2024-02-07 16:31:26 +0530

    initial commit

--- script.js
+++ script.js
@@ -24,21 +24,45 @@
         });
 // accordian
 
-  const accTitle = document.querySelector(".acc-title")
-  const accDes = document.querySelector(".acc-des");
+  // const accTitle = document.querySelector(".acc-title")
+  // const accDes = document.querySelector(".acc-des");
 
-      accTitle.addEventListener('click', function () {
-         this.classList.toggle('active')
-         console.log("click")
-       if(accDes.style.display === "block"){
-       accDes.style.display = "none"
-       document.querySelector('.acc-title i').classList.replace('fa-plus', 'fa-xmark');
+  //     accTitle.addEventListener('click', function () {
+  //        this.classList.toggle('active')
+  //        console.log("click")
+  //      if(accDes.style.display === "block"){
+  //      accDes.style.display = "none"
+  //      document.querySelector('.acc-title i').classList.replace('fa-plus', 'fa-xmark');
 
-    }else{
-      accDes.style.display = "block"
-        document.querySelector('.acc-title i').classList.replace('fa-xmark', 'fa-plus');
-        // document.querySelector('.acc-title i').classList.replace('fa-plus', 'fa-xmark');
+  //   }else{
+  //     accDes.style.display = "block"
+  //       document.querySelector('.acc-title i').classList.replace('fa-xmark', 'fa-plus');
+  //       // document.querySelector('.acc-title i').classList.replace('fa-plus', 'fa-xmark');
 
+  //   }
+
+  // })
+
+// Get all elements with class "acc-title"
+const accTitles = document.querySelectorAll(".acc-title");
+
+// Iterate over each "acc-title" and add click event listener
+accTitles.forEach(function (accTitle) {
+  accTitle.addEventListener("click", function () {
+    // Toggle the "active" class on the clicked "acc-title"
+    this.classList.toggle("active");
+    console.log("click");
+
+    // Find the associated "acc-des" for the clicked "acc-title"
+    const accDes = this.nextElementSibling;
+
+    // Toggle the display of "acc-des"
+    if (accDes.style.display === "block") {
+      accDes.style.display = "none";
+      // this.querySelector("i").classList.replace("fa-plus","fa-xmark");
+    } else {
+      accDes.style.display = "block";
+      // this.querySelector("i").classList.replace("fa-xmark","fa-plus");
     }
-
-  })
+  });
+});
